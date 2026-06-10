cb2282a1188cbfeee9d1b5bc7abd870f24ce67b7
ShivaKrishna4808 <shivakrish.tech68@gmail.com>
2026-06-10 16:39:53 -0700

    updated packages

---
+++ Packages_.py
@@ -0,0 +1,18 @@
+def sum(x,y):
+    val = x+y
+    return val
+
+def average(x,y):
+    val = (x+y)/2
+    return val
+
+def power(x,y):
+    val = x**y
+    return val
+
+a = 10
+b= 5
+
+print("Sum:",sum(a,b))
+print("Average:",average(a,b))
+print("power:",power(a,b))
